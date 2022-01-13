050599d899c90ce87bcb60bca7039b3993f827e2
George Tudor <Alexander.George.Tudor@gmail.com>
2022-01-13 16:27:13 -0600

    screenshot modal progress

--- js/resume.js
+++ js/resume.js
@@ -1,5 +1,11 @@
 $(() => {
     $('body').on('click', '.screenshot-button', function () {
+
+        $("screenshot-window").toggle()
+        el = document.getElementById("screenshot-window");
+        el.style.visibility = (el.style.visibility == "hidden") ? "visible" : "hidden";
+    });
+    $('body').on('click', '#screenshot-window', function () {
         console.log('working')
         el = document.getElementById("screenshot-window");
         el.style.visibility = (el.style.visibility == "hidden") ? "visible" : "hidden";
